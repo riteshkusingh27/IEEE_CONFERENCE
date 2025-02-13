13680fa1ba8f8183d790550a98d72033bf9865d2
ritesh <ritesh@gmail.com>
2025-02-14 03:41:27 +0530

    header

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 Landing Page 
 
    --- setting up project struct file using vite and tailwind css for styling 
+   --- background image 
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>Conference page </h1>
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
