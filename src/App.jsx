3aea83fbf9d582896393a37af342831923782c70
ritesh <ritesh@gmail.com>
2025-02-14 03:42:15 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './Components/Header.jsx'
+import Header from './Components/header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
