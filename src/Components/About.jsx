94580986c6fbb65e7492ba268fd0256734c2d8ae
ritesh <ritesh@gmail.com>
2025-02-17 22:07:24 +0530

    orgnlogo

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Footer from './Footer';
+import organizermain from '../Images/organizermain.png'
 
 const About = () => {
   return (
     <section className=" w-full h-full bg-gradient-to-r bg-white from-blue-50 to-blue-100   " id="about">
-      <div className="max-w-5xl mx-auto ">
-        <h2 className="text-3xl md:text-4xl font-bold text-blue-900 mb-6">About the Conference</h2>
+      <div className="max-w-5xl mx-auto pt-5">
+        <h2 className="text-3xl md:text-4xl font-bold text-blue-900 mb-6 ">About the Conference</h2>
         <p className="text-xl text-black mb-4 font-semibold">
         The IEEE Circuits and Systems Society is the leading organization that promotes the advancement of the theory, analysis, design, tools, and implementation of circuits and systems.The field spans their theoretical foundations, applications, and architectures, as well as circuits and systems implementation of algorithms for signal and information processing. <br />
 
@@ -26,13 +27,21 @@ The Society shall aid in promoting close cooperation and exchange of technical i
 
   
 
-          <div className="bg-none shadow-lg rounded-2xl p-6 hover:shadow-xl transition bg-green-300 ">
-            <h3 className="text-2xl font-bold text-blue-800 mb-3">Organizers</h3>
-            <ul className=" text-black">
+          <div className="bg-none shadow-lg rounded-2xl p-6 hover:shadow-xl transition bg-white-500">
+            <h3 className="text-2xl font-bold text-blue-800 mb-3 flex justify-center">Organizers</h3>
+           <div className="parts flex flex-grow justify-start ">
+           <div className="ul">
+           <ul className=" text-black">
               <li className ='text-xl font-semibold '>Department of Electronics And Communication </li>
               <li className ='text-2xl font-semibold '>Dayananda Sagar University SOE Harohalli </li>
               <li>Chief Patreon <br/><span className='font-bold'>Dr. Arun Balodi </span> <br />Head of Deapartment </li>
             </ul>
+           </div>
+           <div className="logos ml-30">
+               <img src={organizermain} alt="" className='h-30' />
+           </div>
+            
+           </div>
           </div>
         </div>
       </div>
