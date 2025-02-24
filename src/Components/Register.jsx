9e5361dd130553d4a8f0bf06be4b6a439f0745f5
ritesh <ritesh@gmail.com>
2025-02-24 20:44:22 +0530

    responsive

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from './Footer.jsx';
+
 const registrationData = [
     { title: "Early Bird IEEE Student (Indian)", price: 3500 },
     { title: "Early Bird IEEE Member (Indian)", price: 5000 },
@@ -9,29 +10,30 @@ const registrationData = [
     { title: "Regular IEEE Member (Indian)", price: 6500 },
     { title: "Regular Non-IEEE Student (Indian)", price: 7500 },
     { title: "Regular Non-IEEE Member (Indian)", price: 9500 },
-  ];
-    //  div grid for the the registercoloumn 
-  const Register = () => {
+];
+
+const Register = () => {
     return (
-      <div className="main ">
-        <div className=" text-black py-12 px-6">
-        <h2 className="text-4xl font-bold text-center mb-8 text-orange-500">Registrations</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 max-w-6xl mx-auto">
-          {registrationData.map((item, index) => (
-            <div key={index} className="border border-black p-6 rounded-lg text-center">
-              <h3 className="text-lg font-semibold mb-4">{item.title}</h3>
-              <p className="text-2xl font-bold mb-4">₹ {item.price}</p>
-              <button className="text-balck border border-black px-4 py-2 rounded-3xl hover:bg-white hover:text-blue-900">
-                REGISTER NOW
-              </button>
+        <div className="main">
+            <div className="text-black py-12 px-4 sm:px-6">
+                <h2 className="text-4xl font-bold text-center mb-8 text-orange-500">
+                    Registrations
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+                    {registrationData.map((item, index) => (
+                        <div key={index} className="border border-black p-6 rounded-lg text-center">
+                            <h3 className="text-lg font-semibold mb-4">{item.title}</h3>
+                            <p className="text-2xl font-bold mb-4">₹ {item.price}</p>
+                            <button className="text-black border border-black px-4 py-2 rounded-3xl hover:bg-white hover:text-blue-900">
+                                REGISTER NOW
+                            </button>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
+            <Footer />
         </div>
-      </div>
-      <Footer/>
-      </div>
     );
-  };
-  
-  export default Register;
-  
+};
+
+export default Register;
